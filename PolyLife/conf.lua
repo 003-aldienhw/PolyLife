8d4f55262e3a7d03c7ce6f0ea99d8a4a1c27478d
AldienHW <aldienhaidarwahyono@gmail.com>
2026-08-23 21:19:44 +0700

    Initial commit

---
+++ PolyLife/conf.lua
@@ -0,0 +1,8 @@
+function lovr.conf(t)
+    t.window.width = 1000
+    t.window.height = 640
+    t.window.centered = true
+    t.window.fullscreen = false
+    t.window.resizable = true
+    t.window.title = 'PolyLife'
+end
---
+++ PolyLife/main.lua
@@ -0,0 +1,86 @@
+local function grid(size, subdivisions)
+  local vertices = {}
+  local step = size / (subdivisions - 1)
+  for z = -size / 2, size / 2, step do
+    for x = -size / 2, size / 2, step do
+      table.insert(vertices, {x, 0, z})
+      table.insert(vertices, {x, 0, z + step})
+      table.insert(vertices, {x + step, 0, z})
+      table.insert(vertices, {x, 0, z + step})
+      table.insert(vertices, {x + step, 0, z + step})
+      table.insert(vertices, {x + step, 0, z})
+    end
+  end
+  local water_vertices = {}
+  local step = size / (subdivisions - 1)
+  for z = -size / 2, size / 2, step do
+    for x = -size / 2, size / 2, step do
+      table.insert(water_vertices, {x, 0, z})
+      table.insert(water_vertices, {x, 0, z + step})
+      table.insert(water_vertices, {x + step, 0, z})
+      table.insert(water_vertices, {x, 0, z + step})
+      table.insert(water_vertices, {x + step, 0, z + step})
+      table.insert(water_vertices, {x + step, 0, z})
+    end
+  end
+  return vertices, water_vertices
+end
+
+local function terrain_fn(x, z)
+  return 15 * (lovr.math.noise(x * 0.05, z * 0.05) - 0.5)
+end
+
+local function water(x, z)
+  return 2 * (lovr.math.noise(x * 0.05, z * 0.05) - 0.5)
+end
+
+function lovr.load()
+  size = 100
+  world = lovr.physics.newWorld(0, -9.81, 0, false)
+  lovr.graphics.setBackgroundColor(0x02b2f2)
+  local vertices, water_vertices = grid(size, 100)
+  for vi = 1, #vertices do
+    local x,y,z = unpack(vertices[vi])
+    vertices[vi][2] = terrain_fn(x, z)
+  end
+  for vi = 1, #water_vertices do
+    local x,y,z = unpack(water_vertices[vi])
+    water_vertices[vi][2] = water(x, z)
+  end
+  ground_mesh = lovr.graphics.newMesh(vertices)
+  water_mesh = lovr.graphics.newMesh(water_vertices)
+  world:newTerrainCollider(size, terrain_fn)
+  box_colliders = {}
+end
+
+function lovr.update(dt)
+  if lovr.timer.getTime() % 1 < dt then
+    local collider = world:newBoxCollider(
+      lovr.math.randomNormal(size / 10, 0),
+      lovr.math.randomNormal(1, 20),
+      lovr.math.randomNormal(size / 10, 0),
+      1)
+    table.insert(box_colliders, collider)
+  end
+  world:update(dt)
+end
+
+function lovr.draw(pass)
+  pass:setColor(0x32a852)
+  for _, collider in ipairs(box_colliders) do
+    local x, y, z, angle, ax, ay, az = collider:getPose()
+    pass:cube(x, y, z, 1, angle, ax, ay, az)
+  end
+
+  pass:setColor(0x029c1e)
+  pass:draw(ground_mesh)
+
+  pass:setColor(0x062cb8)
+  pass:draw(water_mesh)
+
+  pass:setWireframe(true)
+  pass:setColor(0x022e0e)
+  pass:draw(ground_mesh)
+  pass:setColor(0x01186e)
+  pass:draw(water_mesh)
+end
